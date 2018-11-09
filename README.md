eb786ee842bab258deace1d8cbd8e6a7d3e62a3f
JuliaVaca <thesimpatic@hotmail.com>
2018-11-09 13:08:00 -0600

    ejercicio3

---
+++ README.md
@@ -0,0 +1,8 @@
+# row_column
+
+A new Flutter project.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+void main() => runApp(new Application());
+class Application extends StatelessWidget {
+ @override
+ Widget build(BuildContext context) {
+ return new MaterialApp(
+ title: 'Container',
+ home: new Scaffold(
+ body: new Row(
+ mainAxisAlignment: MainAxisAlignment.center,
+ children: <Widget>[
+ new Text('this'),
+ new Column(
+ mainAxisAlignment: MainAxisAlignment.center,
+ children: <Widget>[
+ new Text('this'),
+ new Text('is'),
+ new Text('column'),
+ ],
+ ),
+ new Text('Row'),
+ ],
+ )
+ ),
+ );
+ }
+}
